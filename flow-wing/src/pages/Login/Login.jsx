5d23b5ee02aba03cd4517aa7ff2137413ec8448a
Tuğçe Özelmaci <74603980+7tugce@users.noreply.github.com>
2023-12-25 17:27:52 +0300

    validation added to login and register pages

--- flow-wing/src/pages/Login/Login.jsx
+++ flow-wing/src/pages/Login/Login.jsx
@@ -5,47 +5,109 @@ import Header from "../../components/Header";
 import { useNavigate } from "react-router";
 import { Divider, Input } from "antd";
 import { Link } from "react-router-dom";
+import { TextField } from "@mui/material";
+import { Formik, useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
-let navigate = useNavigate()
+  let navigate = useNavigate();
 
-// login handler function
-const handleLogin=()=>{
-  navigate("/home")
-}
+  // login handler function
+  const handleLogin = () => {
+    navigate("/home");
+  };
+  const validationSchema = Yup.object({
+    userName: Yup.string()
+      .required("Zorunlu alan")
+      .min(4, "Kullanıcı adı en az 4 karakterden oluşmalıdır.")
+      .max(8, "Kullanıcı adı en fazla 8 karakterden oluşabilir."),
+    password: Yup.string()
+      .required("Zorunlu alan")
+      .min(4, "Şifre en az 4 karakter içermelidir")
+      .max(8, "Şifre en fazla 8 karakterden oluşabilir."),
+    email: Yup.string()
+      .email("Geçersiz e-mail adresi")
+      .required("Zorunlu alan"),
+
+  });
 
   return (
     <>
-    <Header/>
-        <div className="login-page-content">
-   
+      <Header />
+      <div className="login-page-content">
+    
         <p>Hoş Geldiniz</p>
-        <Divider/>
-      <div className="input-areas">
-        {" "}
-        <label for="username">Kullanıcı Adı</label>
-        <Input type="text" id="username" name="username" required />
-       
-      </div>
+        <Divider />
+        <Formik
+          initialValues={{
+            userName: "",
+            password: "",
+            email: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            console.log(values);
+            navigate("/login");
+          }}
+        >
+          {({ handleSubmit, handleChange, values, errors }) => (
+            <form>
+              <div className="input-areas">
+                {" "}
+                <TextField
+                  name="userName"
+                  id="standard-basic"
+                  label="Kulanıcı Adı"
+                  variant="standard"
+                  onChange={handleChange}
+                  value={values.userName}
+                />
+              </div>
+              {errors.userName && errors.userName}
+              <div className="input-areas">
+                <TextField
+                  id="standard-basic"
+                  name="email"
+                  label="Mail "
+                  variant="standard"
+                  onChange={handleChange}
+                  value={values.email}
+                />
+              </div>
 
-      <div className="input-areas">
-        {" "}
-        <label for="email">Mail</label>
-        <Input type="text" id="email" name="email" required  />
-      </div>
+              {errors.email ? errors.email : null}
+             
+              <div className="input-areas">
+                {" "}
+                <TextField
+                  id="standard-basic"
+                  name="password"
+                  label="Şifre"
+                  variant="standard"
+                  type="password"
+                  onChange={handleChange}
+                  value={values.password}
+                />
+              </div>
+              {errors.password && errors.password}
+             
+              {/* LINK TO REGISTER PAGE IF USER DOESN'T HAVE AN ACCOUNT */}
+              <div  className="register-link">
+          Hesabınız yok mu? <Link to="/register">Kayıt Ol</Link>
+        </div>
 
-      <div className="input-areas">
-        {" "}
-        <label for="password">Şifre</label>
-        <Input.Password className="input-password" required  />
+              {/* SUBMIT BUTTON */}
+
+              <button className="sign-in-btn" onClick={handleSubmit}>
+              Giriş Yap
+              </button>
+            </form>
+          )}
+        </Formik>
+      
+      
       </div>
-      <div>
-  Hesabınız yok mu? <Link to="/register">Kayıt Ol</Link>
-</div>
-      <button className="sign-in-btn" onClick={handleLogin}>Giriş Yap</button>
-    </div>
     </>
-  
   );
 };
 
--- flow-wing/src/pages/Register/Register.jsx
+++ flow-wing/src/pages/Register/Register.jsx
@@ -1,55 +1,125 @@
-import { Divider, Input } from 'antd'
-import React from 'react'
-import { useNavigate } from 'react-router'
-import { Link } from 'react-router-dom'
-import "./register.css"
+import { Divider, Input } from "antd";
+import React from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import "./register.css";
+import { TextField } from "@mui/material";
+import { Formik, useFormik } from "formik";
+import * as Yup from "yup";
 
 const Register = () => {
+  let navigate = useNavigate();
 
-  let navigate = useNavigate()
+  // register handler function
+  // const handleRegister = () => {
+  //   navigate("/login");
+  // };
 
-// register handler function
-const handleRegister=()=>{
-  navigate("/login")
-}
+  // YUP VALIDATION SCHEMA
+  const validationSchema = Yup.object({
+    userName: Yup.string()
+      .required("Zorunlu alan")
+      .min(4, "Kullanıcı adı en az 4 karakterden oluşmalıdır.")
+      .max(8, "Kullanıcı adı en fazla 8 karakterden oluşabilir."),
+    password: Yup.string()
+      .required("Zorunlu alan")
+      .min(4, "Şifre en az 4 karakter içermelidir")
+      .max(8, "Şifre en fazla 8 karakterden oluşabilir."),
+    email: Yup.string()
+      .email("Geçersiz e-mail adresi")
+      .required("Zorunlu alan"),
+    passwordConfirmation: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Şifreler eşleşmiyor.")
+      .required("Zorunlu alan"),
+  });
+
   return (
     <>
-  
-        <div className="register-page-content">
-   
+      <div className="register-page-content">
         <p>Hoş Geldiniz</p>
-        <Divider/>
-      <div className="input-areas">
-        {" "}
-        <label for="username">Kullanıcı Adı</label>
-        <Input type="text" id="username" name="username" required />
-       
-      </div>
+        <Divider />
+        <Formik
+          initialValues={{
+            userName: "",
+            password: "",
+            email: "",
+            passwordConfirmation: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            console.log(values);
+            navigate("/login");
+          }}
+        >
+          {({ handleSubmit, handleChange, values, errors }) => (
+            <form>
+              <div className="input-areas">
+                {" "}
+                <TextField
+                  name="userName"
+                  id="standard-basic"
+                  label="Kulanıcı Adı"
+                  variant="standard"
+                  onChange={handleChange}
+                  value={values.userName}
+                />
+              </div>
+              {errors.userName && errors.userName}
+              <div className="input-areas">
+                <TextField
+                  id="standard-basic"
+                  name="email"
+                  label="Mail "
+                  variant="standard"
+                  onChange={handleChange}
+                  value={values.email}
+                />
+              </div>
 
-      <div className="input-areas">
-        {" "}
-        <label for="email">Mail</label>
-        <Input type="text" id="email" name="email" required  />
-      </div>
+              {errors.email ? errors.email : null}
+            
+              <div className="input-areas">
+                {" "}
+                <TextField
+                  id="standard-basic"
+                  name="password"
+                  label="Şifre"
+                  variant="standard"
+                  type="password"
+                  onChange={handleChange}
+                  value={values.password}
+                />
+              </div>
+              {errors.password && errors.password}
+              <div className="input-areas">
+                {" "}
+                <TextField
+                  id="standard-basic"
+                  label="Şifre Tekrar"
+                  variant="standard"
+                  type="password"
+                  name="passwordConfirmation"
+                  onChange={handleChange}
+                  value={values.passwordConfirmation}
+                />
+              </div>
+              {errors.passwordConfirmation && errors.passwordConfirmation}
+              {/* LINK TO LOGIN PAGE IF USER HAS AN ACCOUNT */}
+              <div className="register-link">
+                Hesabınız var mı? <Link to="/login">Giriş Yap</Link>
+              </div>
 
-      <div className="input-areas">
-        {" "}
-        <label for="password">Şifre</label>
-        <Input.Password className="input-password" required  />
-      </div>
+              {/* SUBMIT BUTTON */}
 
-      <div className="input-areas">
-        {" "}
-        <label for="password">Şifre Tekrar</label>
-        <Input.Password className="input-password" required  />
+              <button className="register-btn" onClick={handleSubmit}>
+                Hesap Oluştur
+              </button>
+            </form>
+          )}
+        </Formik>
       </div>
-<div>
-  Hesabınız var mı? <Link to="/login">Giriş Yap</Link>
-</div>
-      <button className="register-btn" onClick={handleRegister}>Hesap Oluştur</button>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
